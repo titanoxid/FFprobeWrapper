1eb020631c5c77cb3a11f1087e6de18caa3c5bab
titanoxid <dustishostile@gmail.com>
2019-05-22 14:25:16 +0200

    added models for ffprobe and metadatainfo

---
+++ FFprobeWrapper.Library/Models/FFprobe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.IO;
+
+namespace FFprobeWrapper.Library.Models
+{
+    public class FFprobe
+    {
+        public string ExecutionPath { get; private set; }
+
+        public FFprobe(string path)
+        {
+            ExecutionPath = GetExecutionPath(path);
+        }
+
+        private string GetExecutionPath(string path)
+        {
+            return Path.Combine(path, "ffprobe");
+        }
+
+        public Process GetProcess()
+        {
+            Process process = new Process();
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.FileName = ExecutionPath;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = false;
+            return process;
+        }
+    }
+}
---
+++ FFprobeWrapper.Library/Models/MetadataInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FFprobeWrapper.Library.Models
+{
+    public class MetadataInfo
+    {
+        public string Dimension { get; set; }
+        public TimeSpan Duration { get; set; }
+        public double Framerate { get; set; }
+        public uint Bitrate { get; set; }
+        public string VideoCodec { get; set; }
+        public string AudioCodec { get; set; }
+    }
+}
